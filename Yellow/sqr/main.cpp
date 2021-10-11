8781d79025e25fb38e321bc8039595411a252979
follow39 <>
2021-10-11 21:25:53 +0300

    Create main.cpp

---
+++ Yellow/sqr/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+
+
+
+int main() {
+    vector<int> v = {1, 2, 3};
+    cout << "vector:";
+    for (int x : Sqr(v)) {
+        cout << ' ' << x;
+    }
+    cout << endl;
+
+    map<int, pair<int, int>> map_of_pairs = {
+        {4, {2, 2}},
+        {7, {4, 3}}
+    };
+    cout << "map of pairs:" << endl;
+    for (const auto& x : Sqr(map_of_pairs)) {
+        cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+    }
+
+    return 0;
+}
