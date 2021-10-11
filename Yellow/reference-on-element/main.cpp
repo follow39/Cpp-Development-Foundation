3e13d6a6e3248058f2d6c722e52e12df420f1fe1
follow39 <>
2021-10-11 21:56:49 +0300

    Update main.cpp

--- Yellow/reference-on-element/main.cpp
+++ Yellow/reference-on-element/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+template <class Key, class Value>
+Value& GetRefStrict(map<Key, Value>& input, Key key) {
+    if(input.count(key) == 0) {
+        throw runtime_error("");
+    }
+    return input[key];
+}
+
+int main() {
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl; // выведет newvalue
     return 0;
 }
