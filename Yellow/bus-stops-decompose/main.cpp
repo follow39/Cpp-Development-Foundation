6486e02a26cee0b91b2968afa07b614e5c29b4e7
follow39 <>
2021-10-12 20:39:25 +0300

    Update main.cpp

--- Yellow/bus-stops-decompose/main.cpp
+++ Yellow/bus-stops-decompose/main.cpp
@@ -19,7 +19,38 @@ struct Query {
     vector<string> stops;
 };
 
+istream& operator >> (istream& is, QueryType& q) {
+    string cmd;
+    is >> cmd;
+    if(cmd == "NEW_BUS") {
+        q = QueryType::NewBus;
+    } else if(cmd == "BUSES_FOR_STOP") {
+        q = QueryType::BusesForStop;
+    } else if(cmd == "STOPS_FOR_BUS") {
+        q = QueryType::StopsForBus;
+    } else if(cmd == "ALL_BUSES") {
+        q = QueryType::AllBuses;
+    }
+    return is;
+}
+
+istream& operator >> (istream& is, vector<string>& vec) {
+    string temp;
+    while(is >> temp) {
+        vec.push_back(temp);
+    }
+    return is;
+}
+
 istream& operator >> (istream& is, Query& q) {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+
+    is >> type >> bus >> stop >> stops;
+    q = {type, bus, stop, stops};
+
     return is;
 }
 
@@ -90,79 +121,79 @@ int main() {
 
 
 
-    int q;
-    cin >> q;
+//    int q;
+//    cin >> q;
 
-    map<string, vector<string>> buses_to_stops, stops_to_buses;
+//    map<string, vector<string>> buses_to_stops, stops_to_buses;
 
-    for (int i = 0; i < q; ++i) {
-        string operation_code;
-        cin >> operation_code;
+//    for (int i = 0; i < q; ++i) {
+//        string operation_code;
+//        cin >> operation_code;
 
-        if (operation_code == "NEW_BUS") {
+//        if (operation_code == "NEW_BUS") {
 
-            string bus;
-            cin >> bus;
-            int stop_count;
-            cin >> stop_count;
-            vector<string>& stops = buses_to_stops[bus];
-            stops.resize(stop_count);
-            for (string& stop : stops) {
-                cin >> stop;
-                stops_to_buses[stop].push_back(bus);
-            }
+//            string bus;
+//            cin >> bus;
+//            int stop_count;
+//            cin >> stop_count;
+//            vector<string>& stops = buses_to_stops[bus];
+//            stops.resize(stop_count);
+//            for (string& stop : stops) {
+//                cin >> stop;
+//                stops_to_buses[stop].push_back(bus);
+//            }
 
-        } else if (operation_code == "BUSES_FOR_STOP") {
+//        } else if (operation_code == "BUSES_FOR_STOP") {
 
-            string stop;
-            cin >> stop;
-            if (stops_to_buses.count(stop) == 0) {
-                cout << "No stop" << endl;
-            } else {
-                for (const string& bus : stops_to_buses[stop]) {
-                    cout << bus << " ";
-                }
-                cout << endl;
-            }
+//            string stop;
+//            cin >> stop;
+//            if (stops_to_buses.count(stop) == 0) {
+//                cout << "No stop" << endl;
+//            } else {
+//                for (const string& bus : stops_to_buses[stop]) {
+//                    cout << bus << " ";
+//                }
+//                cout << endl;
+//            }
 
-        } else if (operation_code == "STOPS_FOR_BUS") {
+//        } else if (operation_code == "STOPS_FOR_BUS") {
 
-            string bus;
-            cin >> bus;
-            if (buses_to_stops.count(bus) == 0) {
-                cout << "No bus" << endl;
-            } else {
-                for (const string& stop : buses_to_stops[bus]) {
-                    cout << "Stop " << stop << ": ";
-                    if (stops_to_buses[stop].size() == 1) {
-                        cout << "no interchange";
-                    } else {
-                        for (const string& other_bus : stops_to_buses[stop]) {
-                            if (bus != other_bus) {
-                                cout << other_bus << " ";
-                            }
-                        }
-                    }
-                    cout << endl;
-                }
-            }
+//            string bus;
+//            cin >> bus;
+//            if (buses_to_stops.count(bus) == 0) {
+//                cout << "No bus" << endl;
+//            } else {
+//                for (const string& stop : buses_to_stops[bus]) {
+//                    cout << "Stop " << stop << ": ";
+//                    if (stops_to_buses[stop].size() == 1) {
+//                        cout << "no interchange";
+//                    } else {
+//                        for (const string& other_bus : stops_to_buses[stop]) {
+//                            if (bus != other_bus) {
+//                                cout << other_bus << " ";
+//                            }
+//                        }
+//                    }
+//                    cout << endl;
+//                }
+//            }
 
-        } else if (operation_code == "ALL_BUSES") {
+//        } else if (operation_code == "ALL_BUSES") {
 
-            if (buses_to_stops.empty()) {
-                cout << "No buses" << endl;
-            } else {
-                for (const auto& bus_item : buses_to_stops) {
-                    cout << "Bus " << bus_item.first << ": ";
-                    for (const string& stop : bus_item.second) {
-                        cout << stop << " ";
-                    }
-                    cout << endl;
-                }
-            }
+//            if (buses_to_stops.empty()) {
+//                cout << "No buses" << endl;
+//            } else {
+//                for (const auto& bus_item : buses_to_stops) {
+//                    cout << "Bus " << bus_item.first << ": ";
+//                    for (const string& stop : bus_item.second) {
+//                        cout << stop << " ";
+//                    }
+//                    cout << endl;
+//                }
+//            }
 
-        }
-    }
+//        }
+//    }
 
     return 0;
 }
