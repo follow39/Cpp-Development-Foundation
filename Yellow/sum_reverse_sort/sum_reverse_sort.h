a03b59f049df1b46d34140f13853f1b5ca67b075
follow39 <>
2021-10-16 10:00:56 +0300

    Commit

---
+++ Yellow/sum_reverse_sort/sum_reverse_sort.h
@@ -0,0 +1,11 @@
+#ifndef SUM_REVERSE_SORT_H
+#define SUM_REVERSE_SORT_H
+
+#include <string>
+#include <vector>
+
+int Sum(int x, int y);
+std::string Reverse(std::string s);
+void Sort(std::vector<int>& nums);
+
+#endif // SUM_REVERSE_SORT_H
