958719ee249dca526e3805bf657ed1df7f32778b
follow39 <>
2021-10-16 18:16:57 +0300

    Update main.cpp

--- Yellow/remove-duplicates/main.cpp
+++ Yellow/remove-duplicates/main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 template <typename T>
 void RemoveDuplicates(vector<T>& elements) {
     sort(elements.begin(), elements.end());
-    auto it = unique(elements.begin(), elements.end());
-    elements.erase(it, elements.end());
+    elements.erase(unique(elements.begin(), elements.end()), elements.end());
 }
 
 int main() {
