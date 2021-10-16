b396148977d6d3ad15f062951b6eaf36e201babf
follow39 <>
2021-10-16 10:42:20 +0300

    Commit

---
+++ Yellow/rectangle/rectangle.h
@@ -0,0 +1,16 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle {
+public:
+    Rectangle(int width, int height);
+    int GetArea() const;
+    int GetPerimeter() const;
+    int GetWidth() const;
+    int GetHeight() const;
+
+private:
+    int width_, height_;
+};
+
+#endif // RECTANGLE_H
