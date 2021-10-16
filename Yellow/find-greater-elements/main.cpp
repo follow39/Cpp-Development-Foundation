0cd98f1267b459c82818c4460421dc568cf2ef56
follow39 <>
2021-10-16 13:29:43 +0300

    Create main.cpp

---
+++ Yellow/find-greater-elements/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+    return vector<T>(find_if(elements.begin(), elements.end(), [border](const T& x) { return x > border; }), elements.end());
+}
+
+int main()
+{
+    for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+      cout << x << " ";
+    }
+    cout << endl;
+
+    string to_find = "Python";
+    cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+    return 0;
+}
