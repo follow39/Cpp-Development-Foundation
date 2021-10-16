47bd42ea29a49039ad3ea493345ac9520b9c21dd
follow39 <>
2021-10-16 13:11:59 +0300

    Create main.cpp

---
+++ Yellow/print-vector-part/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void PrintVectorPart(const vector<int>& numbers) {
+    auto it = find_if(numbers.begin(), numbers.end(), [](int n) { return n < 0;} );
+
+    while (it != numbers.begin()) {
+        --it;
+        cout << *it << " ";
+    }
+}
+
+int main() {
+    PrintVectorPart({6, 1, 8, -5, 4});
+    cout << endl;
+    PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+    cout << endl;
+    PrintVectorPart({6, 1, 8, 5, 4});
+    cout << endl;
+    return 0;
+}
