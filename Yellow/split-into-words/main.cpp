52ada954c93ac7b30ccc2f9a642482db63b66577
follow39 <>
2021-10-16 13:51:09 +0300

    Update main.cpp

--- Yellow/split-into-words/main.cpp
+++ Yellow/split-into-words/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<string> SplitIntoWords(const string& s);
+vector<string> SplitIntoWords(const string& s) {
+    vector<string> result;
+    auto it = s.begin();
+    auto temp = s.begin();
+
+    while(it != s.end()) {
+        temp = find(it, s.end(), ' ');
+        result.push_back({it, temp});
+        it = temp;
+    }
+
+    return result;
+}
 
 int main() {
-  string s = "C Cpp Java Python";
+    string s = "C Cpp Java Python";
 
-  vector<string> words = SplitIntoWords(s);
-  cout << words.size() << " ";
-  for (auto it = begin(words); it != end(words); ++it) {
-    if (it != begin(words)) {
-      cout << "/";
+    vector<string> words = SplitIntoWords(s);
+    cout << words.size() << " ";
+    for (auto it = begin(words); it != end(words); ++it) {
+        if (it != begin(words)) {
+            cout << "/";
+        }
+        cout << *it;
     }
-    cout << *it;
-  }
-  cout << endl;
+    cout << endl;
 
-  return 0;
+    return 0;
 }
