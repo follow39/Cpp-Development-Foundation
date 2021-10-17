02f6ddcbe051755e3dfa3141348882ac3057147b
follow39 <>
2021-10-17 21:40:04 +0300

    Create main.cpp

---
+++ Yellow/find-nearest-element/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+set<int>::const_iterator FindNearestElement(
+    const set<int>& numbers,
+        int border) {
+    set<int>::const_iterator result = numbers.begin();
+
+    for(auto it = numbers.begin(); it != numbers.end(); ++it) {
+        if(abs(*it - border) < abs(*result - border)) {
+            result = it;
+        }
+    }
+
+    return result;
+}
+
+
+int main() {
+  set<int> numbers = {1, 4, 6};
+  cout <<
+      *FindNearestElement(numbers, 0) << " " <<
+      *FindNearestElement(numbers, 3) << " " <<
+      *FindNearestElement(numbers, 5) << " " <<
+      *FindNearestElement(numbers, 6) << " " <<
+      *FindNearestElement(numbers, 100) << endl;
+
+  set<int> empty_set;
+
+  cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
+  return 0;
+}
