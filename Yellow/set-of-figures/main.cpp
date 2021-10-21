82acfdcbceaec2c7bab34040766ce05b3c3256cf
follow39 <>
2021-10-21 11:57:59 +0300

    Update main.cpp

--- Yellow/set-of-figures/main.cpp
+++ Yellow/set-of-figures/main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <memory>
+#include <iomanip>
 
 using namespace std;
 
 class Figure {
 public:
     virtual string Name() const = 0;
-    virtual int Perimeter () const = 0;
-    virtual int Area() const = 0;
+    virtual double Perimeter () const = 0;
+    virtual double Area() const = 0;
 protected:
     string name;
-    int perimeter;
+    double perimeter;
     double area;
 };
 
 class Triangle : public Figure {
 public:
-    Triangle(int a, int b, int c) {
+    Triangle(double a, double b, double c) {
         name = "Triangle";
         perimeter = a + b + c;
-        double p = static_cast<double>(perimeter) / 2;
-        area = sqrt(p) * (p - a) * (p - b) * (p - c);
+        double p = perimeter / 2;
+        area = sqrt(p * (p - a) * (p - b) * (p - c));
     }
     string Name() const override {
         return name;
     }
-    int Perimeter() const {
+    double Perimeter() const override {
         return perimeter;
     }
-    int Area() const {
+    double Area() const override {
         return area;
     }
 };
 
 class Rectangle : public Figure {
 public:
-    Rectangle(int width, int height) {
+    Rectangle(double width, double height) {
         name = "Rectangle";
         perimeter = width * 2 + height * 2;
         area = width * height;
@@ -44,17 +47,17 @@ public:
     string Name() const override {
         return name;
     }
-    int Perimeter() const {
+    double Perimeter() const override {
         return perimeter;
     }
-    int Area() const {
+    double Area() const override {
         return area;
     }
 };
 
 class Circle : public Figure {
 public:
-    Circle(int r) {
+    Circle(double r) {
         name = "Rectangle";
         perimeter = 2 * 3.14 * r;
         area = 3.14 * pow(r, 2);
@@ -62,14 +65,37 @@ public:
     string Name() const override {
         return name;
     }
-    int Perimeter() const {
+    double Perimeter() const override {
         return perimeter;
     }
-    int Area() const {
+    double Area() const override {
         return area;
     }
 };
 
+shared_ptr<Figure> CreateFigure(istream& is) {
+    shared_ptr<Figure> result;
+    string type;
+    is >> type;
+    if(type == "TRIANGLE") {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        is >> a >> b >> c;
+        result = make_shared<Triangle>(a, b, c);
+    } else if(type == "RECT") {
+        int width = 0;
+        int height = 0;
+        is >> width >> height;
+        result = make_shared<Rectangle>(width, height);
+    } else if(type == "CIRCLE") {
+        int r = 0;
+        is >> r;
+        result = make_shared<Circle>(r);
+    }
+
+    return result;
+}
 
 int main() {
     vector<shared_ptr<Figure>> figures;
