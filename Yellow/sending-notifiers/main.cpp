e8b60ee1b50a42506780daffab73a2aa7bc99c68
follow39 <>
2021-10-21 11:19:28 +0300

    Update main.cpp

--- Yellow/sending-notifiers/main.cpp
+++ Yellow/sending-notifiers/main.cpp
@@ -4,30 +4,55 @@
 using namespace std;
 
 void SendSms(const string& number, const string& message) {
-  cout << "Send '" << message << "' to number " << number << endl;
+    cout << "Send '" << message << "' to number " << number << endl;
 }
 
 void SendEmail(const string& email, const string& message) {
-  cout << "Send '" << message << "' to e-mail "  << email << endl;
+    cout << "Send '" << message << "' to e-mail "  << email << endl;
 }
 
 /*
  Реализуйте здесь классы INotifier, SmsNotifier, EmailNotifier
  */
 class INotifier {
+public:
+    virtual void Notify(const string& message) const = 0;
+};
 
+class SmsNotifier : public INotifier {
+public:
+    SmsNotifier(string new_number)
+        :number(new_number) {}
+    void Notify(const string& message) const override {
+        SendSms(number, message);
+    }
+
+private:
+    const string number;
+};
+
+class EmailNotifier : public INotifier {
+public:
+    EmailNotifier(string new_email)
+        :email(new_email) {}
+    void Notify(const string& message) const override {
+        SendEmail(email, message);
+    }
+
+private:
+    const string email;
 };
 
 void Notify(INotifier& notifier, const string& message) {
-  notifier.Notify(message);
+    notifier.Notify(message);
 }
 
 
 int main() {
-  SmsNotifier sms{"+7-495-777-77-77"};
-  EmailNotifier email{"na-derevnyu@dedushke.ru"};
+    SmsNotifier sms{"+7-495-777-77-77"};
+    EmailNotifier email{"na-derevnyu@dedushke.ru"};
 
-  Notify(sms, "I have White belt in C++");
-  Notify(email, "And want a Yellow one");
-  return 0;
+    Notify(sms, "I have White belt in C++");
+    Notify(email, "And want a Yellow one");
+    return 0;
 }
