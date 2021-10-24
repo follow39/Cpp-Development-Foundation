23ce0b5ba98ac23b32ab9c54f0e5626b958c00fd
follow39 <>
2021-10-24 23:43:45 +0300

    Update main.cpp

--- Yellow/yellow-final/main.cpp
+++ Yellow/yellow-final/main.cpp
@@ -133,5 +133,5 @@ void TestAll() {
     tr.RunTest(TestDatabase, "Test database from GitHub");
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-//    std::cout << duration << endl;
+    std::cout << duration << endl;
 }
