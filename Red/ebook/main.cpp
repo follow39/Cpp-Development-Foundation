fb4b591977edaddb40ec0204c3b8c685ea6e039d
follow39 <>
2021-10-27 00:14:19 +0300

    Update main.cpp

--- Red/ebook/main.cpp
+++ Red/ebook/main.cpp
@@ -1,105 +1,91 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 #include <utility>
 
 using namespace std;
 
+
 class ReadingManager {
 public:
-  ReadingManager()
-      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+    ReadingManager()
+        : users(MAX_USER_COUNT_ + 1, 0) {}
 
-  void Read(int user_id, int page_count) {
-    if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
-    }
-    user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
-    }
-  }
+    void Read(int user_id, int page_count) {
+        if(page_count == 0) {
+            return;
+        }
 
-  double Cheer(int user_id) const {
-    if (user_page_counts_[user_id] == 0) {
-      return 0;
+        int temp = users[user_id];
+        if(temp != 0) {
+            --puc_map[temp];
+            if(puc_map[temp] == 0) {
+                puc_map.erase(temp);
+            }
+        }
+        users[user_id] = page_count;
+        ++puc_map[page_count];
+        users_set.insert(user_id);
     }
-    const int user_count = GetUserCount();
-    if (user_count == 1) {
-      return 1;
+
+    double Cheer(int user_id) const {
+        int currentUserPageCount = users[user_id];
+        if(currentUserPageCount == 0 || users_set.size() == 0) {
+            return 0;
+        }
+        if(users_set.size() == 1) {
+            return 1;
+        }
+
+        int sum = 0;
+        for(const auto& p : puc_map) {
+            if(p.first >= currentUserPageCount) {
+                break;
+            }
+            sum += p.second;
+        }
+
+        return static_cast<double>(sum) / (users_set.size() - 1);
     }
-    const int page_count = user_page_counts_[user_id];
-    int position = user_positions_[user_id];
-    while (position < user_count &&
-      user_page_counts_[sorted_users_[position]] == page_count) {
-      ++position;
-    }
-    if (position == user_count) {
-        return 0;
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - position) * 1.0 / (user_count - 1);
-  }
 
 private:
-  // Статическое поле не принадлежит какому-то конкретному
-  // объекту класса. По сути это глобальная переменная,
-  // в данном случае константная.
-  // Будь она публичной, к ней можно было бы обратиться снаружи
-  // следующим образом: ReadingManager::MAX_USER_COUNT.
-  static const int MAX_USER_COUNT_ = 100'000;
+    static const int MAX_USER_COUNT_ = 100'000;
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
-
-  int GetUserCount() const {
-    return sorted_users_.size();
-  }
-  void AddUser(int user_id) {
-    sorted_users_.push_back(user_id);
-    user_positions_[user_id] = sorted_users_.size() - 1;
-  }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-  }
+    vector<int> users;
+    map<int, int> puc_map;
+    set<int> users_set;
 };
 
 
 int main() {
-  // Для ускорения чтения данных отключается синхронизация
-  // cin и cout с stdio,
-  // а также выполняется отвязка cin от cout
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    // Для ускорения чтения данных отключается синхронизация
+    // cin и cout с stdio,
+    // а также выполняется отвязка cin от cout
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  ReadingManager manager;
+    ReadingManager manager;
 
-  int query_count;
-  cin >> query_count;
+    int query_count;
+    cin >> query_count;
 
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-    int user_id;
-    cin >> user_id;
+    for (int query_id = 0; query_id < query_count; ++query_id) {
+        string query_type;
+        cin >> query_type;
+        int user_id;
+        cin >> user_id;
 
-    if (query_type == "READ") {
-      int page_count;
-      cin >> page_count;
-      manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+        if (query_type == "READ") {
+            int page_count;
+            cin >> page_count;
+            manager.Read(user_id, page_count);
+        } else if (query_type == "CHEER") {
+            cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
