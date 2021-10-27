6f75d594dc3927650885cc20c3d830666c7badf8
follow39 <>
2021-10-27 14:17:48 +0300

    Update main.cpp

--- Red/hotel/main.cpp
+++ Red/hotel/main.cpp
@@ -9,89 +9,84 @@ using namespace std;
 
 struct Client {
     int64_t client_id;
-    int room_count;
+    int64_t room_count;
+    int64_t time;
+};
+struct BookUnit {
+    string hotel_name;
+    int64_t client_id;
+    int64_t room_count;
     int64_t time;
 };
 
 class HotelSystem {
 public:
-    void Book(int64_t client_id, int room_count, const string& hotel_name, int64_t time) {
-        hotels[hotel_name].push({client_id, room_count, time});
+    void Book(int64_t time, const string& hotel_name, int64_t client_id, int64_t room_count) {
         ++unique_clients[hotel_name][client_id];
         lastTime = time;
         rooms_reserved[hotel_name] += room_count;
-        isUpdated = false;
-        update_hotels.insert(hotel_name);
+        allBooks.push({hotel_name, client_id, room_count, time});
     }
+
     int64_t Clients(const string& hotel_name) {
-        if(!isUpdated) {
-            RefreshAllHotels();
-        }
+        RefreshData();
         return unique_clients[hotel_name].size();
     }
-    int64_t Rooms(string& hotel_name) {
-        auto& clients = hotels[hotel_name];
-        RefreshHotel(hotel_name, clients);
+
+    int64_t Rooms(const string& hotel_name) {
+        RefreshData();
         return rooms_reserved[hotel_name];
     }
+
 private:
     static const int SECONDS_IN_DAY = 86400;
 
-    void RefreshAllHotels() {
-        isUpdated = true;
-        for(auto& hotel : update_hotels) {
-            RefreshHotel(hotel, hotels[hotel]);
-        }
-        update_hotels.clear();
-    }
+    void RefreshData() {
+        while(!allBooks.empty() && allBooks.front().time <= (lastTime - SECONDS_IN_DAY)) {
+            auto& hotel_name = allBooks.front().hotel_name;
+            auto& client_id = allBooks.front().client_id;
+            auto& room_count = allBooks.front().room_count;
 
-    void RefreshHotel(const string& hotel_name, queue<Client>& clients) {
-        while(!clients.empty() && clients.front().time <= (lastTime - SECONDS_IN_DAY)) {
-            auto& uc = unique_clients[hotel_name][clients.front().client_id];
-            --uc; // O(1)
-            if(uc == 0) {
-                unique_clients[hotel_name].erase(clients.front().client_id);
+            --unique_clients[hotel_name][client_id];
+            if(unique_clients[hotel_name][client_id] == 0) {
+                unique_clients[hotel_name].erase(client_id);
             }
-            rooms_reserved[hotel_name] -= clients.front().room_count;
-            clients.pop();
+            rooms_reserved[hotel_name] -= room_count;
+            allBooks.pop();
         }
     }
 
-    bool isUpdated = false;
     int64_t lastTime = 0;
-    map<string, queue<Client>> hotels;
-    map<string, map<int64_t, int>> unique_clients;
-    map<string, int> rooms_reserved;
-    set<string> update_hotels;
+    queue<BookUnit> allBooks;
+    map<string, map<int64_t, int64_t>> unique_clients; // map<hotel_name, map<client_id, client_books>>
+    map<string, int64_t> rooms_reserved; // map<hotel_name, rooms_reserved>
 };
 
 int main()
 {
-    int q = 0;
-    cin >> q;
-
     HotelSystem hs;
 
     int64_t client_id;
-    int room_count;
+    int64_t room_count;
     string hotel_name;
     int64_t time;
     string cmd;
+
+    int q = 0;
+    cin >> q;
     while(q-- > 0) {
         cin >> cmd;
         if(cmd == "BOOK") {
             cin >> time >> hotel_name >> client_id >> room_count;
-            hs.Book(client_id, room_count, hotel_name, time);
+            hs.Book(time, hotel_name, client_id, room_count);
         } else if(cmd == "CLIENTS") {
             cin >> hotel_name;
-            cout << hs.Clients(hotel_name) << '\n';
+            cout << hs.Clients(hotel_name) << endl;
         } else if(cmd == "ROOMS") {
             cin >> hotel_name;
-            cout << hs.Rooms(hotel_name) << '\n';
+            cout << hs.Rooms(hotel_name) << endl;
         }
-
     }
-
 
     return 0;
 }
