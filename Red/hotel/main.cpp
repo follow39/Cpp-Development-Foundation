30c433e7652dd2db4eb37576c8b80d5eced57546
follow39 <>
2021-10-27 10:40:47 +0300

    Update main.cpp

--- Red/hotel/main.cpp
+++ Red/hotel/main.cpp
@@ -3,28 +3,29 @@
 #include <queue>
 #include <set>
 #include <string>
+#include <cstdint>
 
 using namespace std;
 
 struct Client {
-    long client_id;
+    int64_t client_id;
     int room_count;
-    long long time;
+    int64_t time;
 };
 
 class HotelSystem {
 public:
-    void Book(long client_id, int room_count, const string& hotel_name, long long time) {
+    void Book(int64_t client_id, int room_count, const string& hotel_name, int64_t time) {
         hotels[hotel_name].push({client_id, room_count, time});
         ++unique_clients[hotel_name][client_id];
         lastTime = time;
         rooms_reserved[hotel_name] += room_count;
     }
-    long Clients(const string& hotel_name) {
+    int64_t Clients(const string& hotel_name) {
         RefreshAllHotels();
         return unique_clients[hotel_name].size();
     }
-    long Rooms(string& hotel_name) {
+    int64_t Rooms(string& hotel_name) {
         auto& clients = hotels[hotel_name];
         RefreshHotel(hotel_name, clients);
         return rooms_reserved[hotel_name];
@@ -39,10 +40,7 @@ private:
     }
 
     void RefreshHotel(const string& hotel_name, queue<Client>& clients) {
-        if(clients.size() == 0) {
-            return;
-        }
-        while(clients.front().time <= (lastTime - SECONDS_IN_DAY)) {
+        while(!clients.empty() && clients.front().time <= (lastTime - SECONDS_IN_DAY)) {
             --unique_clients[hotel_name][clients.front().client_id];
             if(unique_clients[hotel_name][clients.front().client_id] == 0) {
                 unique_clients[hotel_name].erase(clients.front().client_id);
@@ -53,9 +51,9 @@ private:
 
     }
 
-    long long lastTime = 0;
+    int64_t lastTime = 0;
     map<string, queue<Client>> hotels;
-    map<string, map<long, int>> unique_clients;
+    map<string, map<int64_t, int>> unique_clients;
     map<string, int> rooms_reserved;
 };
 
@@ -66,10 +64,10 @@ int main()
 
     HotelSystem hs;
 
-    long client_id;
+    int64_t client_id;
     int room_count;
     string hotel_name;
-    long long time;
+    int64_t time;
     string cmd;
     while(q-- > 0) {
         cin >> cmd;
