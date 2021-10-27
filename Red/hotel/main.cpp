bdc5df68db676ec8415be89d72cfca01233ad919
follow39 <>
2021-10-27 09:29:07 +0300

    Create main.cpp

---
+++ Red/hotel/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+class HotelSystem {
+
+    void Book(int client_id, int room_count, string hotel_name, int time) {
+
+    }
+    int Clients(string hotel_name) {
+        return 0;
+    }
+    int Rooms(string hotel_name) {
+        return 0;
+    }
+private:
+    static const int SECONDS_IN_DAY = 86400;
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
