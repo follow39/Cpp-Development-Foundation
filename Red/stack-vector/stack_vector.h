66eecb1745f0ad2fffa9ec63dd4d8c70a84817cc
follow39 <>
2021-10-28 21:33:15 +0300

    Commit

---
+++ Red/stack-vector/stack_vector.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <stdexcept>
+using namespace std;
+
+template <typename T, size_t N>
+class StackVector {
+public:
+    explicit StackVector(size_t a_size = 0) {
+        if(a_size > N) {
+            throw invalid_argument("Size > N");
+        }
+        size = a_size;
+    }
+
+    T& operator[](size_t index) {
+        return data[index];
+    }
+    const T& operator[](size_t index) const {
+        return data[index];
+    }
+
+    T* begin() {
+        return data[0];
+    }
+    T* end() {
+        return data[size-1];
+    }
+    const T* begin() const {
+        return data[0];
+    }
+    const T* end() const {
+        return data[size-1];
+    }
+
+    size_t Size() const {
+        return size;
+    }
+    size_t Capacity() const {
+        return N;
+    }
+
+    void PushBack(const T& value) {
+        if(size == N) {
+            throw overflow_error("overflow");
+        }
+        data[size] = value;
+        ++size;
+    }
+    T PopBack() {
+        if(size == 0) {
+            throw underflow_error("underflow");
+        }
+        --size;
+        return data[size];
+    }
+
+private:
+    T data[N];
+    size_t size;
+};
