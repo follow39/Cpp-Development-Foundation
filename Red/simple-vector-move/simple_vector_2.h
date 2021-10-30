5b6de8197c8232b3ba4ce0904a71ebb56a71f09e
follow39 <>
2021-10-30 12:13:57 +0300

    Commit

---
+++ Red/simple-vector-move/simple_vector_2.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cstdlib>
+#include <algorithm>
+#include <iterator>
+
+
+template <typename T>
+class SimpleVector {
+public:
+    SimpleVector() = default;
+    explicit SimpleVector(size_t new_size)
+        : data(new T[new_size]),
+          size(new_size),
+          capacity(new_size) {}
+    SimpleVector(const SimpleVector& other) = delete;
+    ~SimpleVector() {
+        delete [] data;
+    }
+
+    T& operator[](size_t index) {
+        return data[index];
+    }
+
+    T* begin() {
+        return &data[0];
+    }
+    T* end() {
+        return &data[size];
+    }
+    const T* begin() const {
+        return &data[0];
+    }
+    const T* end() const {
+        return &data[size];
+    }
+
+    size_t Size() const {
+        return size;
+    }
+    size_t Capacity() const {
+        return capacity;
+    }
+    void PushBack(T value) {
+        if(capacity == 0) {
+            data = new T[1];
+            capacity = 1;
+        }
+        if(size == capacity) {
+            capacity *= 2;
+            T* new_data = new T[capacity];
+            std::copy(std::make_move_iterator(begin()), std::make_move_iterator(end()), new_data);
+            delete [] data;
+            data = new_data;
+        }
+        data[size] = std::move(value);
+        ++size;
+    }
+    void operator=(const SimpleVector& other) = delete;
+    void operator=(SimpleVector&& other) {
+        delete [] data;
+        data = std::move(other.data);
+        size = other.size;
+        capacity = other.capacity;
+        other = SimpleVector();
+    }
+
+private:
+    T* data;
+    size_t size;
+    size_t capacity;
+};
+
