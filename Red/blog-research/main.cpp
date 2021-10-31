8b2633cf3a0ceafcdec8907a3941a71bcb43a714
follow39 <>
2021-10-31 10:27:16 +0300

    Update main.cpp

--- Red/blog-research/main.cpp
+++ Red/blog-research/main.cpp
@@ -12,7 +12,70 @@
 #include <functional>
 #include <set>
 
+
 using namespace std;
+
+template <typename Iterator>
+class IteratorRange {
+public:
+    IteratorRange(Iterator begin, Iterator end)
+        : first(begin)
+        , last(end)
+        , size_(distance(first, last))
+    {
+    }
+
+    Iterator begin() const {
+        return first;
+    }
+
+    Iterator end() const {
+        return last;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+private:
+    Iterator first, last;
+    size_t size_;
+};
+
+template <typename Iterator>
+class Paginator {
+private:
+    vector<IteratorRange<Iterator>> pages;
+
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (size_t left = distance(begin, end); left > 0; ) {
+            size_t current_page_size = min(page_size, left);
+            Iterator current_page_end = next(begin, current_page_size);
+            pages.push_back({begin, current_page_end});
+
+            left -= current_page_size;
+            begin = current_page_end;
+        }
+    }
+
+    auto begin() const {
+        return pages.begin();
+    }
+
+    auto end() const {
+        return pages.end();
+    }
+
+    size_t size() const {
+        return pages.size();
+    }
+};
+
+template <typename C>
+auto Paginate(C& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
 
 struct Stats {
     map<string, int> word_frequences;
@@ -24,43 +87,31 @@ struct Stats {
     }
 };
 
-Stats ExploreLine(const set<string>& key_words, const string& line) {
+template <typename ContainerOfVectors>
+Stats ExploreKeyWordsSingleThread(const set<string>& key_words, const ContainerOfVectors& words) {
     Stats result;
-    string word;
-    istringstream ss(line);
-    while(ss >> word) {
-        if(key_words.count(word)) {
+    for(const auto& word : words) {
+        if(key_words.count(word) != 0) {
             ++result.word_frequences[word];
         }
     }
     return result;
 }
 
-Stats ExploreKeyWordsSingleThread(const set<string>& key_words, const string& input) {
-    Stats result;
-    istringstream ss(input);
-
-    for(string line; getline(ss, line);) {
-        result += ExploreLine(key_words, line);
-    }
-    return ExploreLine(key_words, input);
-}
-
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
     vector<future<Stats>> futures;
+    vector<string> words;
     Stats result;
 
     while(input) {
         string temp;
-        for(int i = 0; i < 1; ++i) {
-            string line;
-            getline(input, line);
-            if(!line.empty()) {
-                temp += line + ' ';
-            }
-        }
+        input >> temp;
+        words.push_back(temp);
+    }
 
-        futures.push_back(async(ExploreKeyWordsSingleThread, ref(key_words), move(temp)));
+    const auto pages = Paginate(words, 1000);
+    for(const auto& page : pages) {
+        futures.push_back(async([=] { return ExploreKeyWordsSingleThread(key_words, page); }));
     }
 
     for(auto& x : futures) {
