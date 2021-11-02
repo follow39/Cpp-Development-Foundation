b9e7f362f7abb532597f2bb09ae2f820546c3578
follow39 <ingang39@gmail.com>
2021-11-02 21:31:24 +0300

    Commit

--- Red/red-final/search_server_test.cpp
+++ Red/red-final/search_server_test.cpp
@@ -2,9 +2,48 @@
 
 using namespace std;
 
-template<typename ContainerOfVectors>
-DocsContainer::DocsContainer(const ContainerOfVectors &page, const size_t doc_id_start) {
+vector<string> ParseWordsFromDoc(const string& doc) {
+    stringstream ss(doc);
+    vector<string> result;
+    while(ss) {
+        ss >> ws;
+        string temp;
+        ss >> temp;
+        if(!temp.empty()) {
+            result.push_back(temp);
+        }
+    }
+    return result;
+}
 
+template<typename ContainerOfVectors>
+DocsContainer::DocsContainer(const ContainerOfVectors &page, const size_t new_doc_id_start)
+        : doc_id_start(new_doc_id_start) {
+    size_t current_id = 0;
+    for (const auto &doc: page) {
+        auto temp_vector = ParseWordsFromDoc(doc);
+        for(auto& word : temp_vector) {
+            const auto it_word = words_set.insert(word);
+            ++words_map[*it_word.first][current_id];
+        }
+        ++current_id;
+    }
+}
+
+map<size_t, size_t> DocsContainer::GetStats(const string_view word) const {
+    if (words_map.count(word) == 0) {
+        return {};
+    }
+    map<size_t, size_t> result;
+    const auto &docs_count_vector = words_map.at(word);
+
+    for (size_t i = 0; i < docs_count_vector.size(); ++i) {
+        if (docs_count_vector[i] != 0) {
+            result[doc_id_start + i] = docs_count_vector[i];
+        }
+    }
+
+    return result;
 }
 
 SearchServerTest::SearchServerTest(istream &document_input) {
@@ -12,6 +51,9 @@ SearchServerTest::SearchServerTest(istream &document_input) {
 }
 
 void SearchServerTest::UpdateDocumentBase(istream &document_input) {
+    lock_guard<mutex> g(base_update_mutex);
+    auto &base = current_base ? base1 : base0;
+
     vector<string> docs;
     while (document_input) {
         string temp;
@@ -29,6 +71,12 @@ void SearchServerTest::UpdateDocumentBase(istream &document_input) {
         futures.push_back(async([=] { return DocsContainer(page, current_page * page_size); }));
         ++current_page;
     }
+
+    base.clear();
+    for (auto &f: futures) {
+        base.push_back(f.get());
+    }
+    current_base ^= 1;
 }
 
 void SearchServerTest::AddQueriesStream(istream &query_input, ostream &search_results_output) {
--- Red/red-final/search_server_test.h
+++ Red/red-final/search_server_test.h
@@ -9,19 +9,21 @@
 #include <future>
 #include <mutex>
 #include <iterator>
+#include <set>
+#include <sstream>
 
 class DocsContainer {
 public:
-    template <typename ContainerOfVectors>
-     explicit DocsContainer(const ContainerOfVectors& page, const size_t doc_id_start);
+    template<typename ContainerOfVectors>
+    explicit DocsContainer(const ContainerOfVectors &page, const size_t new_doc_id_start);
 
     // const pair<doc_id_start, vector<count>> GetStats(const string_view word) const
+     map<size_t, size_t> GetStats(const string_view word) const; // return map<doc_id, count>
 
 private:
-    // size_t doc_id_start
-    // mutex container_mutex
-    // vector<string> docs
-    // map<string_view, vector<size_t>> - map<word, vector[doc_id]<count>>
+    size_t doc_id_start;
+    set<string> words_set; // all words in this page. it save words for string_view in map
+    map<string_view, vector<size_t>> words_map;  // map<word, vector[doc_id]<count>>
 };
 
 
@@ -38,9 +40,11 @@ public:
 private:
     // size_t current_database {0 , 1}
     // vector<pages> = Paginator(docs, docs.size()/8)
-     vector<DocsContainer> base1;
-     vector<mutex> base1_mutex;
-    // vector<DocsContainer> base2
-    // vector<mutex> base2_mutex
+    size_t current_base = 0;
+    mutex base_update_mutex;
+    vector<DocsContainer> base0;
+    vector<mutex> base0_mutex;
+    vector<DocsContainer> base1;
+    vector<mutex> base1_mutex;
 };
 
