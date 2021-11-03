940a669f5c7c70a5438dd3c06b0b414fad84af16
follow39 <ingang39@gmail.com>
2021-11-03 20:33:29 +0300

    Commit

--- Red/red-final/search_server.h
+++ Red/red-final/search_server.h
@@ -7,39 +7,29 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <future>
-#include <mutex>
-
 using namespace std;
 
 class InvertedIndex {
 public:
-    InvertedIndex& operator=(InvertedIndex &&other)  noexcept;
+  void Add(const string& document);
+  list<size_t> Lookup(const string& word) const;
 
-    void Add(const string& document);
-
-    map<size_t, size_t> Lookup(const string &word);
-
-    const string &GetDocument(size_t id) const {
-        return docs[id];
-    }
+  const string& GetDocument(size_t id) const {
+    return docs[id];
+  }
 
 private:
-    map<string, pair<mutex, map<size_t, size_t>>> index;
-
-    vector<string> docs;
+  map<string, list<size_t>> index;
+  vector<string> docs;
 };
 
 class SearchServer {
 public:
-    SearchServer() = default;
-
-    explicit SearchServer(istream &document_input);
-
-    void UpdateDocumentBase(istream &document_input);
-
-    void AddQueriesStream(istream &query_input, ostream &search_results_output);
+  SearchServer() = default;
+  explicit SearchServer(istream& document_input);
+  void UpdateDocumentBase(istream& document_input);
+  void AddQueriesStream(istream& query_input, ostream& search_results_output);
 
 private:
-    InvertedIndex index;
+  InvertedIndex index;
 };
