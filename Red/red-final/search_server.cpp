300d5db9b4fcf9d6457e7c799867ae6b6e623310
follow39 <ingang39@gmail.com>
2021-11-04 14:12:33 +0300

    Commit

--- Red/red-final/search_server.cpp
+++ Red/red-final/search_server.cpp
@@ -6,6 +6,10 @@
 #include <sstream>
 #include <iostream>
 
+auto Lock(mutex &m) {
+    return lock_guard<mutex>{m};
+}
+
 vector<string> SplitIntoWords(const string &line) {
     istringstream words_input(line);
     return {istream_iterator<string>(words_input), istream_iterator<string>()};
@@ -23,48 +27,73 @@ void SearchServer::UpdateDocumentBase(istream &document_input) {
         ++docs_count;
     }
 
-    index = move(new_index);
+    auto access = GetAccessIndex();
+    access.ref_to_value = move(new_index);
 }
 
-void SearchServer::AddQueriesStream(
-        istream &query_input, ostream &search_results_output
-) {
+void SearchServer::AddQueriesStream(istream &query_input, ostream &search_results_output) {
+    vector<string> input_docs;
     for (string current_query; getline(query_input, current_query);) {
-        const auto words = SplitIntoWords(current_query);
-
-        vector<int> docid_count = vector<int>(docs_count);
-        for (const auto &word: words) {
-            for (const auto& [docid, count]: index.Lookup(word)) {
-                docid_count[docid] += count;
-            }
+        if (!current_query.empty()) {
+            input_docs.push_back(move(current_query));
         }
-
-        vector<pair<int, int>> search_results;
-        search_results.reserve(docs_count);
-        for (int i = 0; i < docs_count; ++i) {
-            if (docid_count[i] != 0) {
-                search_results.emplace_back(i, docid_count[i]);
-            }
-        }
-
-        auto it_mid = Head(search_results, 5ul).end();
-        partial_sort(search_results.begin(), it_mid, search_results.end(),
-                     [](pair<int, int> lhs, pair<int, int> rhs) {
-                         if (lhs.second != rhs.second) {
-                             return lhs.second > rhs.second;
-                         }
-                         return lhs.first < rhs.first;
-                     }
-        );
-
-        search_results_output << current_query << ':';
-        for (auto[docid, hitcount]: Head(search_results, 5)) {
-            search_results_output << " {"
-                                  << "docid: " << docid << ", "
-                                  << "hitcount: " << hitcount << '}';
-        }
-        search_results_output << '\n';
     }
+
+    vector<future<string>> futures;
+    futures.reserve(input_docs.size());
+    for (auto &doc: input_docs) {
+        futures.push_back(async([&] { return AddQueriesThread(move(doc)); }));
+    }
+
+    for(auto& f : futures) {
+        search_results_output << f.get() << '\n';
+    }
+}
+
+string SearchServer::AddQueriesThread(string query_input) {
+    const auto words = SplitIntoWords(query_input);
+
+    vector<int> docid_count = vector<int>(docs_count);
+    for (const auto &word: words) {
+        for (const auto&[docid, count]: index.Lookup(word)) {
+            docid_count[docid] += count;
+        }
+    }
+
+    vector<pair<int, int>> search_results;
+    search_results.reserve(docs_count);
+    for (int i = 0; i < docs_count; ++i) {
+        if (docid_count[i] != 0) {
+            search_results.emplace_back(i, docid_count[i]);
+        }
+    }
+
+    auto it_mid = Head(search_results, 5ul).end();
+    partial_sort(search_results.begin(), it_mid, search_results.end(),
+                 [](pair<int, int> lhs, pair<int, int> rhs) {
+                     if (lhs.second != rhs.second) {
+                         return lhs.second > rhs.second;
+                     }
+                     return lhs.first < rhs.first;
+                 }
+    );
+
+    string result = move(query_input);
+    result += ':';
+    for (auto[docid, hitcount]: Head(search_results, 5)) {
+        result += " {";
+        result += "docid: ";
+        result += to_string(docid);
+        result += ", ";
+        result += "hitcount: ";
+        result += to_string(hitcount);
+        result += '}';
+    }
+    return result;
+}
+
+Access SearchServer::GetAccessIndex() {
+    return Access{Lock(index_mutex), index};
 }
 
 void InvertedIndex::Add(const string &document) {
--- Red/red-final/search_server.h
+++ Red/red-final/search_server.h
@@ -7,6 +7,8 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <future>
+#include <mutex>
 
 using namespace std;
 
@@ -14,7 +16,7 @@ class InvertedIndex {
 public:
     void Add(const string &document);
 
-    const vector<pair<int, int>>& Lookup(const string &word) const;
+    const vector<pair<int, int>> &Lookup(const string &word) const;
 
     const string &GetDocument(int id) const {
         return docs[id];
@@ -24,6 +26,12 @@ private:
     map<string, vector<pair<int, int>>> index;
     vector<string> docs;
     vector<pair<int, int>> empty_vector = {};
+};
+
+
+struct Access {
+    lock_guard<mutex> guard;
+    InvertedIndex &ref_to_value;
 };
 
 class SearchServer {
@@ -36,7 +44,13 @@ public:
 
     void AddQueriesStream(istream &query_input, ostream &search_results_output);
 
+    string AddQueriesThread(string query_input);
+
+    Access GetAccessIndex();
+
 private:
+    mutex index_mutex;
     InvertedIndex index;
     int docs_count = 0;
+//    vector<future<void>> futures_queries;
 };
