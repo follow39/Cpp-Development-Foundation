2dcec41fd6848b783ae9b354138fb366f3424af5
follow39 <ingang39@gmail.com>
2021-12-08 21:49:17 +0300

    transport-directory-san

--- Black/transport-directory-san/main.cpp
+++ Black/transport-directory-san/main.cpp
@@ -6,23 +6,29 @@
 #include "utils.h"
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-  const auto input_doc = Json::Load(cin);
-  const auto& input_map = input_doc.GetRoot().AsMap();
+    fstream fin("input.json");
+    fstream fout("output.json");
 
-  const TransportCatalog db(
-    Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
-    input_map.at("routing_settings").AsMap()
-  );
+    const auto input_doc = Json::Load(fin);
+//    const auto input_doc = Json::Load(cin);
+    const auto &input_map = input_doc.GetRoot().AsMap();
 
-  Json::PrintValue(
-    Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()),
-    cout
-  );
-  cout << endl;
+    const TransportCatalog db(
+            Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
+            input_map.at("routing_settings").AsMap()
+    );
 
-  return 0;
+    Json::PrintValue(
+            Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()),
+            fout
+//            cout
+    );
+    cout << endl;
+
+    return 0;
 }
