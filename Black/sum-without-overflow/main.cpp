ea0d39dad4f0edc4c0c274da5975128adec7c7fc
follow39 <ingang39@gmail.com>
2021-12-11 15:08:39 +0300

    sum-without-overflow

--- Black/sum-without-overflow/main.cpp
+++ Black/sum-without-overflow/main.cpp
@@ -3,13 +3,23 @@
 
 using namespace std;
 
+
+bool func(int64_t a, int64_t b) {
+    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        return true;
+    } else if (a < 0 && b < 0) {
+        return (INT64_MIN - a) <= b;
+    }
+    return (INT64_MAX - a) >= b;
+}
+
 int main() {
     int64_t a = 0;
     int64_t b = 0;
 
     cin >> a >> b;
 
-    cout << (((INT64_MAX - a) > b) ? to_string(a + b) : "Overflow!");
+    cout << (func(a, b) ? to_string(a + b) : "Overflow!");
 
 
     return 0;
